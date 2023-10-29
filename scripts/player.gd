dfa37259288d9a48a7d22f24d6843331f8a6a4a9
Gultur <n.kervinio@gmail.com>
2023-10-29 09:00:15 +0100

    added player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed:int = 300
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
 
 	move_and_slide()
+	clamp_position()
+
+func clamp_position() -> void:
+	var window_size: Vector2 = get_viewport_rect().size
+#	if global_position.x < 0:
+#		global_position.x = 0
+#	if global_position.x > window_size.x:
+#		global_position.x = window_size.x
+#	if global_position.y < 0:
+#		global_position.y = 0
+#	if global_position.y > window_size.y:
+#		global_position.y = window_size.y
+		
+#	global_position.x = clampf(global_position.x, 0, window_size.x)
+#	global_position.y = clampf(global_position.y, 0, window_size.y)
+	
+	global_position = global_position.clamp(Vector2.ZERO, window_size)

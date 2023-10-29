4cec10646becbe337b2dac89bf339029b1f65e47
Gultur <n.kervinio@gmail.com>
2023-10-29 10:50:00 +0100

    added rocket scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,32 @@ extends CharacterBody2D
 class_name Player
 
 var speed:int = 300
+var rocket_scene: PackedScene = preload("res://scenes/rocket.tscn") as PackedScene # for autocomplete
 
+@onready var rocket_spawn_point: Marker2D = $RocketSpawnPoint
+
+# using a Node container to avoid rocket movin g when player is moving . No "transform" property
+# could be in the level node
+@onready var rocket_container: Node = $RocketContainer 
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 
 func _physics_process(_delta):
 	velocity = Vector2.ZERO
+	move()
+	clamp_position()
+
+func shoot() -> void:
+	var rocket: Area2D = rocket_scene.instantiate()
+	
+	rocket.global_position = rocket_spawn_point.global_position
+	rocket_container.add_child(rocket)
+
+func move() -> void:
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
@@ -17,7 +38,7 @@ func _physics_process(_delta):
 		velocity.y = -speed
 
 	move_and_slide()
-	clamp_position()
+
 
 func clamp_position() -> void:
 	var window_size: Vector2 = get_viewport_rect().size
